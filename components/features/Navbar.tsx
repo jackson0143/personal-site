503087e2f07d5f98e0d0b72805370fceb19c7afa
jackson0143 <161844130+jackson0143@users.noreply.github.com>
2025-10-29 21:25:32 +1100

    added footer, updated fields for projhect and experience

--- components/features/Navbar.tsx
+++ components/features/Navbar.tsx
@@ -1,40 +1,30 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 
 
 export default function Navbar() {
   return (
-    <NavigationMenu>
+    <div className="flex items-center justify-between w-full">
+      <span className="font-semibold">Jackson N</span>
+      <NavigationMenu>
         <NavigationMenuList>
-      <NavigationMenuItem>
-
-        
-        <NavigationMenuLink>
-          <Link href="/">Home</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-
-
-      <NavigationMenuItem>
-        <NavigationMenuLink >
-          <Link href="/about">About</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-
-      <NavigationMenuItem>
-        <NavigationMenuLink>
-          <Link href="/contact">Contact</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <Link href="/">Home</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <Link href="/about">About</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <Link href="/contact">Contact</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
   );
 }
