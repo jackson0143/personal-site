fdc924a872780c89f418b7e827b5d02b409de09b
jackson0143 <161844130+jackson0143@users.noreply.github.com>
2025-11-02 23:00:28 +1100

    added the about me page, added colour to the headers

--- components/features/Navbar.tsx
+++ components/features/Navbar.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 
 export default function Navbar() {
+  const pathname = usePathname();
+
+  const linkClass = (href: string) => (pathname === href ? "text-cyan-600" : "");
+
   return (
     <div className="flex items-center justify-between w-full">
       <span className="font-semibold">Jackson Nguyen</span>
@@ -10,17 +17,17 @@ export default function Navbar() {
         <NavigationMenuList>
           <NavigationMenuItem>
             <NavigationMenuLink>
-              <Link href="/">Home</Link>
+              <Link href="/" className={linkClass("/")} >Home</Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink>
-              <Link href="/about">About</Link>
+              <Link href="/about" className={linkClass("/about")} >About</Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink>
-              <Link href="/contact">Contact</Link>
+              <Link href="/contact" className={linkClass("/contact")}>Contact</Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
