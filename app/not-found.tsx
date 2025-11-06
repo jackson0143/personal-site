96ec616970e4783182f98e2cf74664f77d11e20c
jackson0143 <161844130+jackson0143@users.noreply.github.com>
2025-11-06 12:55:48 +1100

    added the not found and loading page. replaced core compoennts with shadcn ones

---
+++ app/not-found.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-6 py-24 text-center">
+      <div>
+        <p className="text-muted-foreground">404</p>
+        <h1 className="mt-1 text-4xl font-semibold leading-tight main-header">Page not found</h1>
+        <p className="mt-3 text-muted-foreground">
+          Sorry, this might be a mistake on my part :(
+        </p>
+      </div>
+
+      <div className="flex flex-wrap items-center justify-center gap-3">
+        <Button asChild>
+          <Link href="/">Go to Home</Link>
+        </Button>
+      </div>
+    </section>
+  );
+}
+
+
