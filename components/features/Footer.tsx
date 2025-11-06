4de024c1d81123546721ae6c904e218a092dba0f
jackson0143 <jacksonn0143@gmail.com>
2025-11-06 13:05:23 +1100

    udpated the footer logos for github, linkedin

--- components/features/Footer.tsx
+++ components/features/Footer.tsx
@@ -7,12 +7,17 @@ export default function Footer() {
   return (
     <footer className="py-8">
       <ul className="flex flex-row items-center">
+
         <Link href="https://github.com/jackson0143/" className="pr-5" aria-label="GitHub" >
-          <Image src="/github-mark.png" alt="github" width={24} height={24} />
+          <Image src="/github-mark.svg" alt="github" width={24} height={24} className="block dark:hidden" />
+          <Image src="/github-mark-white.svg" alt="github" width={24} height={24} className="hidden dark:block" />
         </Link>
+
         <Link href="https://www.linkedin.com/in/jackson-nguyen-0307a6230/" className="pr-5" aria-label="LinkedIn" >
-          <Image src="/linkedin.svg" alt="linkedin" width={24} height={24} />
+          <Image src="/InBug-Black.png" alt="linkedin" width={24} height={24} className="block dark:hidden" />
+          <Image src="/InBug-White.png" alt="linkedin" width={24} height={24} className="hidden dark:block" />
         </Link>
+        
         <Link href="mailto:jacksonn0143@gmail.com" aria-label="Email">
           <Mail className="w-6 h-6" />
         </Link>
